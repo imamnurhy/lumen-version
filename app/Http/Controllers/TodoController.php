80f9e812bb256e555bdbd7fc55249b230c7324ca
imamnurhy <imamnurhy@gmail.com>
2021-08-12 14:01:53 +0700

    first commit

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class TodoController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function index()
+    {
+        return response()->json([
+            'status' => 1,
+            'message' => 'Succesfully'
+        ], 200);
+    }
+}
